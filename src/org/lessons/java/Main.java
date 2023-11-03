e85528d38ab03d6db2318ebb3adb00738cee9769
GianbaBotassi <gianba89@gmail.com>
2023-11-03 15:29:46 +0100

    Bonus done

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -33,6 +33,56 @@ public class Main {
                 e.printStackTrace();
                 System.out.println("Errore PrepareStatement");
             }
+            System.out.println("Seleziona il codice nazione che vuoi visualizzare.");
+            int choice2 = Integer.parseInt(scanner.nextLine());
+
+            String query2 = "SELECT c.name, GROUP_CONCAT(l.`language`, \" \") as lingue \n" +
+                    "FROM countries c \n" +
+                    "JOIN regions r on c.region_id = r.region_id \n" +
+                    "JOIN continents co ON r.continent_id = co.continent_id\n" +
+                    "join country_languages cl on cl.country_id = c.country_id \n" +
+                    "join languages l on cl.language_id = l.language_id \n" +
+                    "where c.country_id = ? \n" +
+                    "group by c.name";
+
+            try(PreparedStatement ps = conn.prepareStatement(query2)){
+                ps.setInt(1, choice2);
+                try(ResultSet rs = ps.executeQuery()){
+                    while(rs.next()){
+                        System.out.printf("Dettagli nazione: %s\nLingue: %s\n",rs.getString("name"),rs.getString("lingue")
+
+                                );
+                    }
+                }catch (SQLException e){
+                    e.printStackTrace();
+                    System.out.println("Errore esecuzione query");
+                }
+            }catch (SQLException e){
+                e.printStackTrace();
+                System.out.println("Errore PrepareStatement");
+            }
+            String query3 = "SELECT cs.`year` ,cs.population ,cs.gdp \n" +
+                    "FROM countries c\n" +
+                    "join country_stats cs on c.country_id = cs.country_id \n" +
+                    "where c.country_id = '107'\n" +
+                    "order by cs.year desc\n" +
+                    "limit 1\n";
+
+            try(PreparedStatement ps = conn.prepareStatement(query3)){
+                try(ResultSet rs = ps.executeQuery()){
+                    while(rs.next()){
+                        System.out.printf("Statistiche recenti:\nYear: %d\nPopulation: %d\nGDP: %d",rs.getInt("year"),rs.getLong("population"),rs.getLong("gdp")
+                        );
+                    }
+                }catch (SQLException e){
+                    e.printStackTrace();
+                    System.out.println("Errore esecuzione query");
+                }
+            }catch (SQLException e){
+                e.printStackTrace();
+                System.out.println("Errore PrepareStatement");
+            }
+
         }catch(SQLException e){
             e.printStackTrace();
             System.out.println("Errore connessione db");
